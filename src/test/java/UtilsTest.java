393e93bbb3cba81b2fc56d102cc2c5286c973a31
Stephen Sandini <stsandini@gmail.com>
2025-12-03 23:11:09 -0600

    Added PIT to pom.xml and corrected Utils

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -5,15 +5,13 @@ class UtilsTest {
 
   @Test
   void testCheckName() {
-    Utils service = new Utils();
-    
     Student s1 = new Student("Alice", 20, 3.3);
     Student s2 = new Student("", 0, 0.0);
     Student s3 = new Student(null, 0, 0.0);
 
-    assertTrue(service.checkName(s1.getName()));
-    assertFalse(service.checkName(s2.getName()));
-    assertFalse(service.checkName(s3.getName()));
+    assertTrue(Utils.checkName(s1.getName()));
+    assertFalse(Utils.checkName(s2.getName()));
+    assertFalse(Utils.checkName(s3.getName()));
   }
 
 
@@ -22,14 +20,12 @@ class UtilsTest {
         else return true;  // Bug: Allows age > 120
   */
   void testIsValidAge() {
-   Utils service = new Utils();
-    
     Student s1 = new Student("Alice", 20, 3.3);
     Student s2 = new Student("", 120, 0.0);
     Student s3 = new Student(null, -1, 0.0);
 
-    assertTrue(service.isValidAge(s1.age));
-    assertTrue(service.isValidAge(s2.age));
-    assertFalse(service.isValidAge(s3.age));
+    assertTrue(Utils.isValidAge(s1.age));
+    assertTrue(Utils.isValidAge(s2.age));
+    assertFalse(Utils.isValidAge(s3.age));
   }
 }
